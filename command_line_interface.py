8cbaf51177be409a092107d5c789403ece702759
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-09-02 12:44:59 -0300

    feat: automatically named results

--- command_line_interface.py
+++ command_line_interface.py
@@ -24,6 +24,7 @@ from rich import print as rprint
 
 from fundamentus.main.fundamentus_pipeline import \
     FundamentusPipeline as Fundamentus
+from fundamentus.utils.indicator_names import INDICATOR_NAME
 
 
 def main(ticker: str) -> dict:
@@ -78,38 +79,38 @@ if __name__ == '__main__':
     # Print the information.
     rprint('[bold blue]Cotação[/bold blue]')
     for key, value in cotacao.items():
-        rprint(f'\t{key}: {value}')
+        rprint(f'\t{INDICATOR_NAME[key]}: {value}')
 
     rprint('\n[bold blue]Informações Básicas[/bold blue]')
     for key, value in informacoes_basicas.items():
-        rprint(f'\t{key}: {value}')
+        rprint(f'\t{INDICATOR_NAME[key]}: {value}')
 
     rprint('\n[bold blue]Oscilações[/bold blue]')
     for key, value in oscilacoes.items():
-        rprint(f'\t{key}: {value}')
+        rprint(f'\t{INDICATOR_NAME[key]}: {value}')
 
     rprint('\n[bold blue]Indicadores de Valuation[/bold blue]')
     for key, value in indicadores_de_valuation.items():
-        rprint(f'\t{key}: {value}')
+        rprint(f'\t{INDICATOR_NAME[key]}: {value}')
 
     rprint('\n[bold blue]Indicadores de Rentabilidade[/bold blue]')
     for key, value in indicadores_de_rentabilidade.items():
-        rprint(f'\t{key}: {value}')
+        rprint(f'\t{INDICATOR_NAME[key]}: {value}')
 
     rprint('\n[bold blue]Indicadores de Endividamento[/bold blue]')
     for key, value in indicadores_de_endividamento.items():
-        rprint(f'\t{key}: {value}')
+        rprint(f'\t{INDICATOR_NAME[key]}: {value}')
 
     rprint('[bold blue]Balanço Patrimonial[/bold blue]')
     for key, value in balanco_patrimonial.items():
-        rprint(f'\t{key}: {value}')
+        rprint(f'\t{INDICATOR_NAME[key]}: {value}')
 
     rprint('\n[bold blue]Demonstrativo de Resultados[/bold blue]')
     rprint('\t[bold blue]Últimos 03 meses[/bold blue]')
     for key, value in demonstrativo_de_resultados['3_meses'].items():
-        rprint(f'\t{key}: {value}')
+        rprint(f'\t{INDICATOR_NAME[key]}: {value}')
 
     rprint('\b[bold blue]Demonstrativo de Resultados[/bold blue]')
     rprint('\t[bold blue]Últimos 12 meses[/bold blue]')
     for key, value in demonstrativo_de_resultados['12_meses'].items():
-        rprint(f'\t{key}: {value}')
+        rprint(f'\t{INDICATOR_NAME[key]}: {value}')
