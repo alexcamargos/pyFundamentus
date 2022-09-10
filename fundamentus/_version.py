38fd0bad0caa1a348b50ad3f50b0e0cebacb5199
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-09-10 06:56:16 -0300

    feat: created file for version control

---
+++ fundamentus/_version.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# ------------------------------------------------------------------------------
+#  Name: _version.py
+#  Version: 0.0.1
+#
+#  Summary: Python Fundamentus
+#           Python Fundamentus is a Python API that allows you to quickly
+#           access the main fundamental indicators of the main stocks
+#           in the Brazilian market.
+#
+#  Author: Alexsander Lopes Camargos
+#  Author-email: alcamargos@vivaldi.net
+#
+#  License: MIT
+# ------------------------------------------------------------------------------
+
+"""
+Python Fundamentus
+
+Python Fundamentus is a Python API that allows you to quickly access the main
+fundamental indicators of the main stocks in the Brazilian market.
+
+"""
+
+__author__ = 'Alexsander Lopes Camargos'
+__email__ = 'alcamargos@vivaldi.net'
+
+# Package version.
+MAJOR = 0
+MINOR = 0
+MICRO = 1
+
+__version__ = f'{MAJOR}.{MINOR}.{MICRO}'
