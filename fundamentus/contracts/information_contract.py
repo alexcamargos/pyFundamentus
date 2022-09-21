f3bb1a6d3e802c657710d368ffe23ee9f1dd875f
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-09-21 06:56:38 -0300

    feat: Implementation of InformationItem as item contract

---
+++ fundamentus/contracts/information_contract.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# ------------------------------------------------------------------------------
+#  Name: information_contract.py
+#  Version: 0.0.1
+#
+#  Summary: Python Fundamentus
+#           Python Fundamentus is a Python API that allows you to quickly
+#           access the main fundamental indicators of the main stocks
+#           in the Brazilian market.
+#
+#  Author: Alexsander Lopes Camargos
+#  Author-email: alcamargos@vivaldi.net
+#
+#  License: MIT
+# ------------------------------------------------------------------------------
+
+"""Information Contract"""
+
+from dataclasses import dataclass
+from decimal import Decimal
+
+
+@dataclass
+class InformationItem:
+    '''Class for keeping track of collected information.'''
+
+    title: str
+    tooltip: str
+    value: Decimal
