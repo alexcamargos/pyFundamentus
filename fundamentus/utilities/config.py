1f33a7aab1a5c2bcb34f1513bcffa6b97ed3960e
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2024-02-13 16:20:03 -0300

    feat: add global configuration variables

---
+++ fundamentus/utilities/config.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# ------------------------------------------------------------------------------
+#  Name: config.py
+#  Version: 0.0.1
+#
+#  Summary: Python Fundamentus
+#           Python Fundamentus is a Python API that allows you to quickly
+#           access the main fundamental indicators of the main stocks
+#           in the Brazilian market.
+#
+#  Author: Alexsander Lopes Camargos
+#  Author-email: alcamargos@vivaldi.net
+#
+#  License: MIT
+# ------------------------------------------------------------------------------
+
+"""Configuration file for the Python Fundamentus API."""
+
+URL = 'https://www.fundamentus.com.br/detalhes.php'
+INTERFACE = 'mobile'
